2ba34cce35948c48833a3e8fff0fea4c81ce84b5
wented6 <44713416+wented6@users.noreply.github.com>
2019-04-06 11:41:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,30 +1,29 @@
 const adminprefix = "!";  
 const devs = ['538676483455975431'];  
 
-hngel.on('message', message => {  
+client.on('message', message => {  
+
   var argresult = message.content.split(` `).slice(1).join(' ');  
     if (!devs.includes(message.author.id)) return;  
-    
-if (message.content.startsWith(adminprefix + 'pl')) {  
-  hngel.user.setGame(argresult);
+
+if (message.content.startsWith(adminprefix + 'plll')) {  
+  client.user.setGame(argresult);
     message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
 } else 
-  if (message.content.startsWith(adminprefix + 'nm')) {
-hngel.user.setUsername(argresult).then
+  if (message.content.startsWith(adminprefix + 'nmmm')) {
+client.user.setUsername(argresult).then
     message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
 return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
 } else
-  if (message.content.startsWith(adminprefix + 'av')) {
-hngel.user.setAvatar(argresult);
+  if (message.content.startsWith(adminprefix + 'avvv')) {
+client.user.setAvatar(argresult);
   message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
       } else     
-if (message.content.startsWith(adminprefix + 'st')) {
-  hngel.user.setGame(argresult, "https://www.twitch.tv/hngel2667");
+if (message.content.startsWith(adminprefix + 'sttt')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/idk");
     message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
 }
 
 });
 
-
-
-client.login(process.env.B);
+  client.login(process.env.B);
