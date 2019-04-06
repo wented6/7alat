d28d64cc2dfd72ed642d9d73ef5c01ba44e1148a
wented6 <44713416+wented6@users.noreply.github.com>
2019-04-06 11:33:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,33 +1,30 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const developers = ['538676483455975431'];
+const adminprefix = "!";  
+const devs = ['538676483455975431'];  
 
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setStatus("dnd")
-});
-
-client.on('message', message => {
-  var argresult = message.content.split(` `).slice(1).join(' ');
-    if (!developers.includes(message.author.id)) return;
-
-if (message.content.startsWith('!pl')) {
-client.user.setActivity(argresult, {type:'Playing'});
-    message.channel.send(` <: :white_check_mark:  **${argresult} \** `)
+hngel.on('message', message => {  
+  var argresult = message.content.split(` `).slice(1).join(' ');  
+    if (!devs.includes(message.author.id)) return;  
+    
+if (message.content.startsWith(adminprefix + 'pl')) {  
+  hngel.user.setGame(argresult);
+    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
 } else 
-if (message.content.startsWith('!st')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/meercy");
-   message.channel.send(` <: :white_check_mark:  **${argresult} \** `)
+  if (message.content.startsWith(adminprefix + 'nm')) {
+hngel.user.setUsername(argresult).then
+    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
+return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
 } else
-if (message.content.startsWith('!ls')) {
-   client.user.setActivity(argresult, {type:'Listening'});
-    message.channel.send(` <: :white_check_mark:  **${argresult} \** `)
-} else
-if (message.content.startsWith('!wt')) {
-   client.user.setActivity(argresult, {type:'Watching'});
-    message.channel.send(` <: :white_check_mark:  **${argresult} \** `)
+  if (message.content.startsWith(adminprefix + 'av')) {
+hngel.user.setAvatar(argresult);
+  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
+      } else     
+if (message.content.startsWith(adminprefix + 'st')) {
+  hngel.user.setGame(argresult, "https://www.twitch.tv/hngel2667");
+    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
 }
+
 });
+
+
 
 client.login(process.env.B);
